f6045ed39c9f14b1a939ad91223af25ea3e04507
Giselle <2014giselle@gmail.com>
2022-09-30 20:09:43 -0400

    Merged Lucy's file into Group 25 file

---
+++ src/Group25/Lucy/OddOrEven.java
@@ -0,0 +1,29 @@
+package Group25.Lucy;
+
+public class OddOrEven {
+    public static void main(String[] args) {
+
+    }
+
+    public static class OddorEven {
+
+         /*
+            1. Numbers -- odd & even
+            Write  a method which can identifies given number is even or odd
+             */
+
+        public static void main(String[] args) {
+
+            System.out.println(oddEven(7));
+        }
+        public static String oddEven(int number){
+
+            String num="";
+            if (number % 2==0){
+                return num="even";
+            }else {
+                return num="odd";
+            }
+        }
+    }
+}
