774dcf0b8ea4e749f51a1e2a1c180b3aa3a0edcc
Giselle <2014giselle@gmail.com>
2022-09-30 20:29:22 -0400

    Created task1

--- src/Group25/Giselle/DivideWithoutOperators.java
+++ src/Group25/Giselle/DivideWithoutOperators.java
@@ -1,15 +1,19 @@
 package Group25.Giselle;
 
-public class DivideWithoutOperator {
+public class DivideWithoutOperators {
     /*
 2.Numbers -- Divide without / operator
 Write a method that can divide two numbers without using division operator.
+
  */
     public static void main(String[] args) {
+
         System.out.println(division(20,5));
     }
+
     public static int division(int dividend, int divisor){
         //     10/2
+
         int result=0;
 
         while(dividend >= divisor){
